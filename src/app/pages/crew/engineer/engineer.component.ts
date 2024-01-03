e0ca606927d6df3d6e332b803008eaeafe2caec9
Ronald Mamani <Ronaldmamani140@gmail.com>
2024-01-03 09:54:58 -0400

    Adicionando as demais paginas de Crew, adicionando o Pilot e Engineer

--- src/app/pages/crew/engineer/engineer.component.ts
+++ src/app/pages/crew/engineer/engineer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavCrewComponent } from '../../../components/nav-crew/nav-crew.component';
+import { PersonInfoComponent } from '../../../components/person-info/person-info.component';
 
 @Component({
   selector: 'app-engineer',
   standalone: true,
-  imports: [],
+  imports: [NavCrewComponent, PersonInfoComponent],
   templateUrl: './engineer.component.html',
   styleUrl: './engineer.component.scss'
 })
--- src/app/pages/crew/pilot/pilot.component.ts
+++ src/app/pages/crew/pilot/pilot.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavCrewComponent } from '../../../components/nav-crew/nav-crew.component';
+import { PersonInfoComponent } from '../../../components/person-info/person-info.component';
 
 @Component({
   selector: 'app-pilot',
   standalone: true,
-  imports: [],
+  imports: [NavCrewComponent, PersonInfoComponent],
   templateUrl: './pilot.component.html',
   styleUrl: './pilot.component.scss'
 })
